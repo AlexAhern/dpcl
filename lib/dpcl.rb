d0a2fcb82cd899c3eb20b5d59704dadbf943d54a
AlexAhern <ac.ahern@yahoo.co.uk>
2016-04-15 16:40:16 +0100

    Updating dpcl.rb

--- lib/dpcl.rb
+++ lib/dpcl.rb
@@ -1,14 +1,15 @@
 require_relative './datafetcher'
-require_relative './jsonparser'
+require_relative './dpjsonparser'
+require_relative './redditpost'
 require 'json'
 
 fetcher = DataFetcher.new
 json = fetcher.fetch("https://api.reddit.com/r/dailyprogrammer/new")
-parser = JsonParser.new(json)
+parser = DPJsonParser.new(json)
 
 
 (0..24).each do |i|
-	puts parser.getPostByIndex(i)["title"]
+	puts parser.getPostAsRedditPost(i).title
 end
 
 #title = title.slice!(24..-1).gsub(/\[|\]/," ").split(" ").map(&:capitalize).join('')
